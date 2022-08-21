dbc8f5fc3a5abcd00164afe453bd02207278483b
Hadicha Nurmamedova <hadicha.nurmamedova@gmail.com>
2022-08-21 18:14:23 +0500

    first commit

---
+++ src/main/java/com/ilearning/finalProject/FinalProjectApplication.java
@@ -0,0 +1,11 @@
+package com.ilearning.finalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectApplication.class, args);
+    }
+}
